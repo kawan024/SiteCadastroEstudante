818553b36ebcc87e0169c903168da2578357e240
kawan024 <kawan.rodrigues.santos@escola.pr.gov.br>
2026-05-05 12:41:42 +0000

    agora sim

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 let form = document.getElementById('cadastroForm');
-
 let lista = document.getElementById('listaAlunos');
 
 form.addEventListener('submit', function(event){
     event.preventDefault()
-    const none = document.getElementById('nomeAluno').Value;
-    const idade = document.getElementById('idadeAluno').Value;
-    const novoItem = documet.getElementById('li');
-    novoItem.innerHTML = `<strong> ${nome} </strong> - ${idade} anos`;
 
-    lista.appemdChild(novoItem);
+    const nome = document.getElementById('nomeAluno').value;
+    const idade = document.getElementById('IdadeAluno').value;
+    const cpf = document.getElementById('cpfAluno').value;
+    const numero = document.getElementById('contatoAluno').value;
 
-    form.requestFullscreen();
+    const novoItem = document.createElement('li');
+    novoItem.innerHTML = `<strong> ${nome} </strong> - ${idade} anos - CPF: ${cpf} - Turma: ${turma} - Contato do Aluno: ${numero}`;
 
+    lista.appendChild(novoItem);
+
+    form.reset();
 });
